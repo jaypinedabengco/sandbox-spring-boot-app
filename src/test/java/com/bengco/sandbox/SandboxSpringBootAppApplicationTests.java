dd657ddc0a61afda2aafd6de9fc31e043422a3e9
Jay Bengco <jaybengco@TRAdmins-MacBook-Pro.local>
2018-07-11 07:54:02 +0800

    ADD integration test

--- src/test/java/com/bengco/sandbox/SandboxSpringBootAppApplicationTests.java
+++ src/test/java/com/bengco/sandbox/SandboxSpringBootAppApplicationTests.java
@@ -1,16 +1,51 @@
 package com.bengco.sandbox;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.bengco.sandbox.model.User;
+import com.bengco.sandbox.service.UserService;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = SandboxSpringBootAppApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
 public class SandboxSpringBootAppApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mvc;
 
+	@MockBean
+	private UserService userService;
+
+	@Test
+	public void givenUsers_whenGetUsers_thenReturnJsonArray() throws Exception {
+
+		User user = new User("controller.test@yopmail.com", "John", "Doe");
+
+		List<User> users = Arrays.asList(user);
+
+		given(userService.getAllUsers()).willReturn(users);
+
+		mvc.perform(get("/api/users").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(jsonPath("$", hasSize(1))).andExpect(jsonPath("$[0].username", is(user.getUsername())));
+	}
 }
